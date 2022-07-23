90bc9ab8ac29a71653a2404a16e7001c39db7049
shatpr <2508509T@student.gla.ac.uk>
2022-07-24 02:09:09 +0800

    updated

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ let api = "https://api.data.gov.sg/v1/environment/psi";
     
     psiReadings = data.items[0].readings.psi_twenty_four_hourly
     regionData = data.region_metadata
-    updatedTimestamp = console.log(new Date(data.items[0].update_timestamp).toLocaleString())
+    updatedTimestamp = new Date(data.items[0].update_timestamp).toLocaleString()
 
-    // document.getElementById("time").innerHTML = updatedTimestamp;
+    document.getElementById("lastupdated").innerHTML = "Last Updated: " + updatedTimestamp;
 
-    // Map
+    // Map  
 
     let tiles = new L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
         detectRetina: true,
@@ -27,6 +27,10 @@ let api = "https://api.data.gov.sg/v1/environment/psi";
     let center = L.bounds([1.56073, 104.11475], [1.16, 103.502]).getCenter()
 
     // Add MaxBound
+    
+    let domain = [51, 101, 201, 301, 401];
+    let psiLevels = ["Good", "Moderate", "Unhealthy", "Very Unhealthy", "Hazardous"];
+
     let map = new L.Map("map", {
         center: [1.347833, 103.809357], 
         zoom: 11,
@@ -38,40 +42,70 @@ let api = "https://api.data.gov.sg/v1/environment/psi";
 
     
         let colorScale = d3.scaleThreshold()
-        .domain([50, 100, 200, 300, 400])
-        .range(["#A0D568, #4FC1E8, #FFCE54, #AC92EB #ED5564"]);
+        .domain(domain)
+        .range(["green", "blue", "yellow", "orange", "red"]);
 
         let legendLabels = d3.scaleThreshold()
-        .domain([50, 100, 200, 300, 400])
-        .range(["Good", "Moderate", "Unhealthy", "Very Unhealthy", "Hazardous"]);
+        .domain(domain)
+        .range(psiLevels);
     
-    for(let i = 0; i < regionData.length; i++) {
-        labelLocation = regionData[i].label_location;
-        region = regionData[i].name;
-        
-        let circle =  L.circleMarker([labelLocation.latitude, labelLocation.longitude], { 
-            radius: labelLocation[region] * 1.5,
-            opacity: .9,                            
-            fillOpacity: 0.3,
-            fillColor: colorScale(labelLocation[region]),
-            color: colorScale(labelLocation[region]),
+    let size = regionData.length;
+   
+    for(let i = 0; i < size; i++) {
+        let labelLocation = regionData[i].label_location;
+        let region = regionData[i].name;
+        console.log(labelLocation.latitude)
+        console.log(labelLocation.longitude)
+        console.log(region)
+        let circle =  L.circle([labelLocation.latitude, labelLocation.longitude], { 
+            radius: psiReadings[region]*40,                      
+            fillOpacity: 0.4,
+            fillColor: colorScale(psiReadings[region]),
+            color: colorScale(psiReadings[region]),
 
-        });
+        }
+        ).addTo(map);
         
 
         let marker = L.marker([labelLocation.latitude, labelLocation.longitude], { 
             icon: new L.DivIcon({
-            html: "<br>" + labelLocation[region] + "</b>",
-            className: 'circle-with-txt'
+            className: 'text-labels',
+            iconAnchor: [13, 16],
+            direction: 'center',
+            html: "<b>" + psiReadings[region] + "</b>",
              })
 
+        }).addTo(map);
+        
+        circle.bindPopup("Status: " + legendLabels(psiReadings[region]), {
+            className: "bd"
         });
-
-        let group = L.layerGroup([circle, marker]);
-        group.addTo(map);
-    }; 
+        marker.bindPopup("Status: " + legendLabels(psiReadings[region]), {
+            className: "bd"
+        });
+    }
     
-    })
+    // Legend
+    let legend = L.control({ position: 'bottomleft' });
+
+    legend.onAdd = function (_map) {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+        levels = [0, 50, 100, 200, 300];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (let i = 0; i < levels.length; i++) {
+            div.innerHTML +=
+            '<div class="mt-1">' + 
+                '<p style="background:' + colorScale(levels[i] + 1) + '"></p> ' + psiLevels[i] +
+            '</div>';
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+})
     
    
 
